cc3b840ed6a91edd34754f0b36b5a795fbe544a7
Ivan Kosarev <ivan@kosarev.info>
2020-05-13 10:28:01 +0300

    Represent key info with a class.

--- zx/_keyboard.py
+++ zx/_keyboard.py
@@ -9,6 +9,24 @@
 #   Published under the MIT license.
 
 
+class KeyInfo(object):
+    def __init__(self, id, index):
+        self.id = id
+        self.index = index  # Left to right, then top to bottom.
+        self.halfrow_index = index // 5
+        self.index_in_halfrow = index % 5
+        self.is_leftside = self.halfrow_index % 2 == 0
+        self.is_rightside = not self.is_leftside
+
+        # Compute port address lines and bit positions.
+        if self.is_leftside:
+            self.address_line = 11 - self.halfrow_index // 2
+            self.port_bit = self.index_in_halfrow
+        else:
+            self.address_line = self.halfrow_index // 2 + 12
+            self.port_bit = 4 - self.index_in_halfrow
+
+
 KEYS_INFO = dict()
 
 # Generate layout-related information.
@@ -17,23 +35,8 @@ layout = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
           'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'ENTER',
           'CAPS SHIFT', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'SYMBOL SHIFT',
           'BREAK SPACE']
-for n, id in enumerate(layout):
-    key = KEYS_INFO.setdefault(id, dict())
-    key['id'] = id
-    key['number'] = n  # Left to right, then top to bottom.
-    key['halfrow_number'] = n // 5
-    key['pos_in_halfrow'] = n % 5
-    key['is_leftside'] = key['halfrow_number'] % 2 == 0
-    key['is_rightside'] = not key['is_leftside']
-
-# Compute port address lines and bit positions.
-for id, key in KEYS_INFO.items():
-    if key['is_leftside']:
-        key['address_line'] = 11 - key['halfrow_number'] // 2
-        key['port_bit'] = key['pos_in_halfrow']
-    else:
-        key['address_line'] = key['halfrow_number'] // 2 + 12
-        key['port_bit'] = 4 - key['pos_in_halfrow']
+for index, id in enumerate(layout):
+    KEYS_INFO[id] = KeyInfo(id, index)
 
 
 class KeyboardState(object):
@@ -63,9 +66,9 @@ class KeyboardState(object):
         return n
 
     def handle_key_stroke(self, key_info, pressed):
-        # print(key_info['id'])
-        addr_line = key_info['address_line']
-        mask = 1 << key_info['port_bit']
+        # print(key_info.id)
+        addr_line = key_info.address_line
+        mask = 1 << key_info.port_bit
 
         if pressed:
             self._state[addr_line - 8] &= mask ^ 0xff
