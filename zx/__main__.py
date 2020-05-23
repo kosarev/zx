f3a8edd2516b4ba5ab82e328f332016870b6c88d
Ivan Kosarev <ivan@kosarev.info>
2020-05-23 13:03:56 +0300

    Support running the emulator via 'python3 -m'.

---
+++ zx/__main__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+#   ZX Spectrum Emulator.
+#   https://github.com/kosarev/zx
+#
+#   Copyright (C) 2017-2019 Ivan Kosarev.
+#   ivan@kosarev.info
+#
+#   Published under the MIT license.
+
+import zx
+
+
+if __name__ == '__main__':
+    zx.main()
