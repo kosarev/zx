5240017711810f21dde9f86f2f6d013567c37257
Ivan Kosarev <mail@ivankosarev.com>
2025-01-18 20:34:56 +0000

    Allow some duration tolerance in the tape-interface example.

--- examples/tape_interface.py
+++ examples/tape_interface.py
@@ -15,6 +15,24 @@ import zx
 SHOW_SCREEN = False
 
 
+class DurationSet(object):
+    def __init__(self):
+        self.__sum = 0
+        self.count = 0
+        self.min = float('+inf')
+        self.max = float('-inf')
+
+    def add(self, duration):
+        self.__sum += duration
+        self.count += 1
+        self.min = min(self.min, duration)
+        self.max = max(self.max, duration)
+
+    @property
+    def average(self):
+        return self.__sum / self.count if self.count > 0 else 0
+
+
 class MySpectrum48(zx.Emulator):
     def __init__(self):
         super().__init__(speed_factor=1 if SHOW_SCREEN else None)
@@ -24,8 +42,7 @@ class MySpectrum48(zx.Emulator):
         self.__last_ear_level = 1
         self.__last_ear_level_tick = 0
         self.__last_half_period_level = 1
-        self.__last_half_period_duration = 0
-        self.__last_half_period_count = 0
+        self.__last_half_period_durations = DurationSet()
 
     def __on_output(self, addr, value):
         if addr & 0xff != 0xfe:
@@ -39,17 +56,28 @@ class MySpectrum48(zx.Emulator):
         tick = self.frame_count * TICKS_PER_FRAME + self.ticks_since_int
         duration = tick - self.__last_ear_level_tick
 
-        if self.__last_half_period_duration == duration:
-            self.__last_half_period_count += 1
-        else:
+        half_period_count = self.__last_half_period_durations.count
+        average_duration = self.__last_half_period_durations.average
+        duration_delta = abs(average_duration - duration)
+        DURATION_TOLERANCE = 10
+        if duration_delta > DURATION_TOLERANCE:
             if self.__report:
-                print(f'Beginning level {self.__last_half_period_level}, '
-                      f'{self.__last_half_period_count} half-period(s) '
-                      f'of {self.__last_half_period_duration} ticks each.')
+                if half_period_count == 1:
+                    print(f'Beginning level {self.__last_half_period_level}, '
+                          f'{half_period_count} half-period '
+                          f'of {average_duration:.0f} ticks.')
+                else:
+                    print(f'Beginning level {self.__last_half_period_level}, '
+                          f'{half_period_count} half-periods '
+                          f'of ~{average_duration:.1f} '
+                          f'({self.__last_half_period_durations.min}-'
+                          f'{self.__last_half_period_durations.max}) '
+                          f'ticks each.')
 
             self.__last_half_period_level = ear_level
-            self.__last_half_period_duration = duration
-            self.__last_half_period_count = 1
+            self.__last_half_period_durations = DurationSet()
+
+        self.__last_half_period_durations.add(duration)
 
         self.__last_ear_level = ear_level
         self.__last_ear_level_tick = tick
@@ -68,7 +96,7 @@ class MySpectrum48(zx.Emulator):
         self.generate_key_strokes('ENTER')
 
         # Run for 5 seconds and report some tape half-periods.
-        super().run(duration=5)
+        super().run(duration=10)
 
 
 def main():
