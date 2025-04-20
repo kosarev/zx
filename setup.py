47c0fe10ab44169fa2b9ca4d53ba331b5fd0b876
Ivan Kosarev <mail@ivankosarev.com>
2025-04-20 09:25:09 +0100

    Attempt to fix CI.

--- setup.py
+++ setup.py
@@ -52,6 +52,11 @@ setup(name='zx',
           'sounddevice',
           'evdev; platform_system=="Linux"',
       ],
+      extras_require={
+          ':"linux" in sys_platform': [
+              'evdev',
+          ],
+      },
       package_data={'zx': ['roms/*', 'py.typed']},
       entry_points={
           'console_scripts': [
